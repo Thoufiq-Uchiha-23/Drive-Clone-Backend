5207bd88f417f31902da5f87ca9407ec4102a088
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-11-14 23:23:38 +0530

    app updated with database

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const userRouter = require('./routes/user.routes')
+const connectToDB = require('./config/db')
+connectToDB()
+
+const dotenv = require('dotenv')
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.use('/user', userRouter)
 
