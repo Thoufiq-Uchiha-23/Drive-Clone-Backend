de4ff393bfaa8b495aa650f07f0f4c7e490c61c2
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-11-20 23:55:58 +0530

    minor change

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -41,6 +41,51 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+// router.post(
+//   "/login",
+//   body("username").trim().isLength({ min: 3 }),
+//   body("password").trim().isLength({ min: 5 }),
+//   async (req, res) => {
+//     const errors = validationResult(req)
+
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({
+//         error: errors.array(),
+//         message: 'Invalid data'
+//       })
+//     }
+
+//     const {username, password} = req.body
+
+//     const user = await userModel.findOne({
+//       username: username
+//     })
+
+//     if(!user){
+//       return res.status(400).json({
+//         message: 'username or password is incorrect'
+//       })
+//     }
+
+//     const isMatch = await bcrypt.compare(password, user.password)
+
+//     if(!isMatch) {
+//       return res.status(400).json({
+//         message: 'username or password is incorrect'
+//       })
+//     }
+
+//     /* jsonwebtoken */
+//     const token = jwt.sign({
+//       userId: user._id,
+//       email: user.email,
+//       username: user.username
+//     },
+//     process.env.JWT_SECRET,
+//   )
+//   }
+// );
+
 router.post(
   "/login",
   body("username").trim().isLength({ min: 3 }),
